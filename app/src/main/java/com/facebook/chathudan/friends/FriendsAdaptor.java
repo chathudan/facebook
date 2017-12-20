f7d89b8b97ebd57fd90a44a20a518bc4b76a5cf6
Chathura Wijesinghe <cdanasiri@gmail.com>
2017-12-20 17:50:25 +0530

    friends list adaptor implemented

--- app/src/main/java/com/facebook/chathudan/friends/FriendsAdaptor.java
+++ app/src/main/java/com/facebook/chathudan/friends/FriendsAdaptor.java
@@ -1,29 +1,66 @@
 package com.facebook.chathudan.friends;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.facebook.chathudan.R;
+import com.facebook.chathudan.data.model.Friend;
+import com.facebook.chathudan.data.network.service.FriendList;
+import com.squareup.picasso.Picasso;
+import java.util.ArrayList;
 
 /**
- * Created by Chathura Wijesinghe <cdanasiri@gmail.com> on 12/20/17.
+ * Facebook friends adaptor
+ *
+ * @author Chathura Wijesinghe (cdanasiri@gmail.com)
  */
 
-public class FriendsAdaptor extends RecyclerView.Adapter<RecyclerView.ViewHolder>  {
+
+public class FriendsAdaptor extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+  private final int TYPE_FRIEND = 0;
+
+  private final ArrayList<Friend> mFriendList;
+  private Context mContext;
+
+  public FriendsAdaptor(ArrayList<Friend> friendsList) {
+    this.mFriendList = friendsList;
+  }
+
   @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+
+    mContext = parent.getContext();
+    LayoutInflater inflater = LayoutInflater.from(mContext);
+    //TODO add loading for pagination / friends limit
+    //if (viewType == TYPE_FRIEND) {
+    return new FriendViewHolder(inflater.inflate(R.layout.item_friend, parent, false));
+    //}
   }
 
   @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    FriendViewHolder friendHolder = ((FriendViewHolder) holder);
+    String friendName = mFriendList.get(position).getName();
+    friendHolder.name.setText(position + "  " + friendName);
+    String avatarUrl = mFriendList.get(position).getPicture();
+    if (avatarUrl != null) {
+      if (avatarUrl.trim().length() > 0) {
+        Picasso.with(mContext).load(avatarUrl).into(friendHolder.avatar);
+      }
+    }
 
+    Animation anim = AnimationUtils.loadAnimation(mContext, android.R.anim.fade_in);
+    friendHolder.name.setAnimation(anim);
   }
 
   @Override public int getItemCount() {
     return 0;
   }
-
 
   public class FriendViewHolder extends RecyclerView.ViewHolder {
     public TextView name;
